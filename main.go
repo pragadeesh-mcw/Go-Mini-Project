3442e7c2fb20628c1aafc4ad8ab259fb3a988948
pragadeesh-mcw <pragadeesh.senthil@multicorewareinc.com>
2024-06-20 14:19:41 +0530

    Revert main.go before library changes
    Library can be pulled from library branch

--- main.go
+++ main.go
@@ -7,16 +7,12 @@ import (
 	"unified/redis_cache"
 )
 
-func Entry() *gin.Engine {
+func main() {
 	//initiate redis and in-memory
 	inMemoryCache := in_memory.NewLRUCache(3, 60)
 	redisCache := redis_cache.NewCache("localhost:6379", "", 0, 3)
 	multiCache := multicache.NewMultiCache(inMemoryCache, redisCache)
 	//setup unified api
-
 	r := api.SetupRouter(multiCache)
-	api.SetupRedisRoutes(r, redisCache)
-
-	api.SetupInMemoryRoutes(r, inMemoryCache)
-	return r
+	r.Run(":8080")
 }
