84be7b0bf6c5d5081f2b26d7e2a85f58f8083838
pragadeesh-mcw <pragadeesh.senthil@multicorewareinc.com>
2024-06-20 13:56:49 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 	"unified/redis_cache"
 )
 
-func entry() {
+func Entry() *gin.Engine {
 	//initiate redis and in-memory
 	inMemoryCache := in_memory.NewLRUCache(3, 60)
 	redisCache := redis_cache.NewCache("localhost:6379", "", 0, 3)
 	multiCache := multicache.NewMultiCache(inMemoryCache, redisCache)
 	//setup unified api
+
 	r := api.SetupRouter(multiCache)
-	r.Run(":8080")
+	api.SetupRedisRoutes(r, redisCache)
+
+	api.SetupInMemoryRoutes(r, inMemoryCache)
+	return r
 }
